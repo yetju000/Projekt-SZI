6ffcf3079a037c610f66edd7e52cfe2e537ab6d6
yetju000 <yetju000steam@gmail.com>
2018-04-06 15:07:40 +0200

    crate and game agent

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+	public GameObject agent;
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - agent.transform.position;
+		transform.position = agent.transform.position + offset;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = agent.transform.position + offset;
+	}
+}
